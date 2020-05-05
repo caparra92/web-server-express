593029ceda19114d91c3cf2449a9051a1b17b8eb
caparra92 <caparra92@gmail.com>
2020-05-05 17:07:46 -0300

    Subiendo proyecto a git

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use( express.static( __dirname + '/public' ) );
+
+//Express HBS engine
+const hbs = require('hbs');
+ 
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+app.set('view engine', 'hbs');
+
+//hbs helpers
+hbs.registerHelper('getAnio', () => {
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+
+  res.render('home', {
+      name: 'Camilo',
+      anio: new Date().getFullYear()
+  });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+  
+  });
+ 
+app.listen(port);
